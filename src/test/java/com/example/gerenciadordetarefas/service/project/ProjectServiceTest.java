5af57d0f796e6c2e0b36bdc518ee60c539530b83
Natalia Barros <natalia.vieiramonteiro70@gmail.com>
2023-10-28 01:56:18 -0300

    Teste da DepartmentService está com todos os métodos passando

---
+++ src/test/java/com/example/gerenciadordetarefas/service/project/ProjectServiceTest.java
@@ -0,0 +1,37 @@
+package com.example.gerenciadordetarefas.service.project;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createProject() {
+    }
+
+    @Test
+    void updateProject() {
+    }
+
+    @Test
+    void getAllProject() {
+    }
+
+    @Test
+    void getProjectById() {
+    }
+
+    @Test
+    void addToAssignedTask() {
+    }
+
+    @Test
+    void deleteProject() {
+    }
+}
